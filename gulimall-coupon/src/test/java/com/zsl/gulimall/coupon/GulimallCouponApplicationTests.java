a2aefe575f9b5ff7fe613dd938e1d988567e4b72
10838837617@qq.com <zsl8315040968zsl>
2020-11-22 22:08:15 +0800

    init

---
+++ gulimall-coupon/src/test/java/com/zsl/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zsl.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
